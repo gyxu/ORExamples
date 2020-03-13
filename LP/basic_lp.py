c4f9b35b5b7db324eb83044a6f9aaf1d6bc53292
gage <gagexgy@outlook.com>
2020-03-13 21:32:35 +0800

    Update the LP code for homework 1-2, 1-3(1)(3)

--- LP/basic_lp.py
+++ LP/basic_lp.py
@@ -1,16 +1,45 @@
 # Harbin Institute of Technology, Shenzhen
-# Dr. XU Gangyan
-# MATH3010 Example 1.5
+# MATH3010
+# Instructor: Dr. XU Gangyan
 
 from scipy import optimize
 import numpy as np
 
-x0_bound = (0, None)
-x1_bound = (0, None)
 
-c = np.array([3, 4])
-A = np.array([[1, 2], [3, 2], [0, 1]])
-b = np.array([6, 12, 2])
+def homework_1_2():
+    c = np.array([700, 600, 600, 300, 350, 250, 900, 800, 700])
+    A = np.array([[-700, -600, -600, 0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, -300, -350, -250, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0, -900, -800, -700],
+                  [1, 0, 0, 1, 0, 0, 1, 0, 0],
+                  [0, 1, 0, 0, 1, 0, 0, 1, 0],
+                  [0, 0, 1, 0, 0, 1, 0, 0, 1]])
+    b = np.array([-130000, -40000, -250000, 200, 400, 600])
 
-result = optimize.linprog(-c, A_ub=A, b_ub=b, bounds=(x0_bound, x1_bound))
-print(result)
+    result = optimize.linprog(-c, A_ub=A, b_ub=b)
+    print(result)
+
+
+def homework_1_3_1():
+    x0_bound = (0, None)
+    x1_bound = (0, None)
+
+    c = np.array([1, 2])
+    A = np.array([[1, 1], [0, 1]])
+    b = np.array([2, 1])
+
+    result = optimize.linprog(-c, A_ub=A, b_ub=b)
+    print(result)
+
+
+def homework_1_3_2():
+    x0_bound = (0, None)
+    x1_bound = (0, None)
+
+    c = np.array([2, -10])
+    A = np.array([[1, -1], [1, -5]])
+    b = np.array([0, -5])
+
+    result = optimize.linprog(c, A_ub=-A, b_ub=-b, bounds=(x0_bound, x1_bound))
+    print(result)
+
